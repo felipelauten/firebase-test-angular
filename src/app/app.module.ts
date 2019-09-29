7099470c04cfeec5b9ba1e32750f42d59f2bb70e
Felipe Christian Lautenschlager <felipelauten@gmail.com>
2019-09-29 22:28:00 +0200

    Firebase Auth login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import * as firebase from 'firebase/app';
+/* Firebase services */
+import {AngularFireModule, FirebaseAppConfig} from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { environment } from '../environments/environment';
+
+/* Auth Service */
+import { AuthenticationService } from './shared/authentication.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../shared/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  email: string;
+  password: string;
+
+  constructor(public authenticationService: AuthenticationService) {}
+
+  signUp() {
+    this.authenticationService.SignUp(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
+
+  signIn() {
+    this.authenticationService.SignIn(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
+
+  signOut() {
+    this.authenticationService.SignOut();
+  }
+
+}
